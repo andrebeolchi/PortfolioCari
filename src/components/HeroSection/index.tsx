2b401a2342eb711b1c4a2a514bca125d561ad695
Andre Beolchi <andregarbini2001@gmail.com>
2024-01-06 10:57:14 -0300

    Add HeaderTabs component and FlyoutMenu component

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,118 +1,9 @@
-import { Dialog } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-
-const navigation = [
-	{ name: "Product", href: "#" },
-	{ name: "Features", href: "#" },
-	{ name: "Marketplace", href: "#" },
-	{ name: "Company", href: "#" }
-];
+import HeaderTabs from "../HeaderTabs";
 
 export default function HeroSection() {
-	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
 	return (
 		<div className="bg-white">
-			<header className="absolute inset-x-0 top-0 z-50">
-				<nav
-					className="flex items-center justify-between p-6 lg:px-8"
-					aria-label="Global">
-					<div className="flex lg:flex-1">
-						<a
-							href="#"
-							className="-m-1.5 p-1.5">
-							<span className="sr-only">Your Company</span>
-							<img
-								className="h-8 w-auto"
-								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-								alt=""
-							/>
-						</a>
-					</div>
-					<div className="flex lg:hidden">
-						<button
-							type="button"
-							className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-							onClick={() => setMobileMenuOpen(true)}>
-							<span className="sr-only">Open main menu</span>
-							<Bars3Icon
-								className="h-6 w-6"
-								aria-hidden="true"
-							/>
-						</button>
-					</div>
-					<div className="hidden lg:flex lg:gap-x-12">
-						{navigation.map((item) => (
-							<a
-								key={item.name}
-								href={item.href}
-								className="text-sm font-semibold leading-6 text-gray-900">
-								{item.name}
-							</a>
-						))}
-					</div>
-					<div className="hidden lg:flex lg:flex-1 lg:justify-end">
-						<a
-							href="#"
-							className="text-sm font-semibold leading-6 text-gray-900">
-							Log in <span aria-hidden="true">&rarr;</span>
-						</a>
-					</div>
-				</nav>
-				<Dialog
-					as="div"
-					className="lg:hidden"
-					open={mobileMenuOpen}
-					onClose={setMobileMenuOpen}>
-					<div className="fixed inset-0 z-50" />
-					<Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-						<div className="flex items-center justify-between">
-							<a
-								href="#"
-								className="-m-1.5 p-1.5">
-								<span className="sr-only">Your Company</span>
-								<img
-									className="h-8 w-auto"
-									src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-									alt=""
-								/>
-							</a>
-							<button
-								type="button"
-								className="-m-2.5 rounded-md p-2.5 text-gray-700"
-								onClick={() => setMobileMenuOpen(false)}>
-								<span className="sr-only">Close menu</span>
-								<XMarkIcon
-									className="h-6 w-6"
-									aria-hidden="true"
-								/>
-							</button>
-						</div>
-						<div className="mt-6 flow-root">
-							<div className="-my-6 divide-y divide-gray-500/10">
-								<div className="space-y-2 py-6">
-									{navigation.map((item) => (
-										<a
-											key={item.name}
-											href={item.href}
-											className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-											{item.name}
-										</a>
-									))}
-								</div>
-								<div className="py-6">
-									<a
-										href="#"
-										className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-										Log in
-									</a>
-								</div>
-							</div>
-						</div>
-					</Dialog.Panel>
-				</Dialog>
-			</header>
+			<HeaderTabs />
 
 			<div className="relative isolate px-6 pt-14 lg:px-8">
 				<div
@@ -132,7 +23,7 @@ export default function HeroSection() {
 							Announcing our next round of funding.{" "}
 							<a
 								href="#"
-								className="font-semibold text-indigo-600">
+								className="font-semibold text-lime-500">
 								<span
 									className="absolute inset-0"
 									aria-hidden="true"
@@ -142,9 +33,7 @@ export default function HeroSection() {
 						</div>
 					</div>
 					<div className="text-center">
-						<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-							Data to enrich your online business
-						</h1>
+						<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Carina C. Pizzo</h1>
 						<p className="mt-6 text-lg leading-8 text-gray-600">
 							Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
 							fugiat veniam occaecat fugiat aliqua.
@@ -152,7 +41,7 @@ export default function HeroSection() {
 						<div className="mt-10 flex items-center justify-center gap-x-6">
 							<a
 								href="#"
-								className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+								className="rounded-md bg-lime-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-lime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-lime-500">
 								Get started
 							</a>
 							<a
