dd9ff593c59f15388963601171a713f688a6654f
Andre Beolchi <andregarbini2001@gmail.com>
2024-01-09 13:31:48 -0300

    Fix conditional class assignment in ProjectsSection.tsx

--- src/views/ProjectsSection.tsx
+++ src/views/ProjectsSection.tsx
@@ -43,7 +43,7 @@ export default function ProjectsSection() {
 					return (
 						<div
 							className={`mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 border-b py-32`}>
-							<div className={`lg:pt-4 ${isEven && "lg:order-last"} ${isLeft}`}>
+							<div className={`lg:pt-4 ${isEven || "lg:order-last"} ${isLeft}`}>
 								<div className="lg:max-w-lg">
 									<h2 className="text-base font-semibold leading-7 text-lime-600 uppercase tracking-wider">
 										{item?.subtitle}
